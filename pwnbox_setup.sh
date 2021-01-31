85b2b8900743032ee2aebdfd21a3915197fee687
Chenyang Yu (ret2basic) <59381775+ret2basic@users.noreply.github.com>
2021-01-31 14:07:43 -0500

    Update pwnbox_setup.sh

--- pwnbox_setup.sh
+++ pwnbox_setup.sh
@@ -21,7 +21,6 @@ python get-pip.py
 rm get-pip.py
 
 # Docker
-<<<<<<< HEAD
 apt --assume-yes update
 apt --assume-yes install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
@@ -29,17 +28,6 @@ apt-key fingerprint 0EBFCD88
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt --assume-yes install docker-ce docker-ce-cli containerd.io
-=======
-sudo apt --assume-yes update
-sudo apt --assume-yes install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt --assume-yes install docker-ce docker-ce-cli containerd.io
-sudo groupadd docker
-sudo usermod -aG docker ${USER}
->>>>>>> 9c1718c140516b87b624f699481c7542472364ab
 
 # Docker Compose
 curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
