323eefe81eabbedf15643afd31e0d892428e542c
Chenyang Yu (ret2basic) <59381775+ret2basic@users.noreply.github.com>
2021-01-31 14:06:16 -0500

    Update pwnbox_setup.sh

--- pwnbox_setup.sh
+++ pwnbox_setup.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 
-# Update and Upgrade
+#-----------------#
+# Install as root |
+#-----------------#
+
+# Switch to root
+sudo su
 cd
-sudo apt --assume-yes update
-sudo apt --assume-yes upgrade
+
+# Update and Upgrade
+apt --assume-yes update
+apt --assume-yes upgrade
 
 # Python
-sudo apt --assume-yes install python-is-python3
+apt --assume-yes install python-is-python3
 
-# Pwntools
-sudo apt --assume-yes update
-sudo apt --assume-yes install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential
-pip3 install --upgrade pip
-pip3 install --upgrade pwntools
+# Pip
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+rm get-pip.py
 
 # Docker
+<<<<<<< HEAD
+apt --assume-yes update
+apt --assume-yes install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt --assume-yes install docker-ce docker-ce-cli containerd.io
+=======
 sudo apt --assume-yes update
 sudo apt --assume-yes install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -24,64 +39,71 @@ sudo apt-get update
 sudo apt --assume-yes install docker-ce docker-ce-cli containerd.io
 sudo groupadd docker
 sudo usermod -aG docker ${USER}
+>>>>>>> 9c1718c140516b87b624f699481c7542472364ab
 
 # Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 # Vim
-sudo apt --assume-yes install vim
+apt --assume-yes install vim
 
 # vimrc
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 # Sublime
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt --assume-yes install apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt --assume-yes update
-sudo apt --assume-yes install sublime-text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
+apt --assume-yes install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
+apt --assume-yes update
+apt --assume-yes install sublime-text
 
 # Powerline fonts
-sudo apt --assume-yes install fonts-powerline
+apt --assume-yes install fonts-powerline
 
 # Zsh
-sudo apt --assume-yes install zsh
+apt --assume-yes install zsh
 
 # tmux
-sudo apt --assume-yes install tmux
+apt --assume-yes install tmux
 
 # Oh My tmux
-sudo apt --assume-yes install git
+apt --assume-yes install git
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
+
+# Pwntools
+apt --assume-yes update
+apt --assume-yes install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential
+pip install --upgrade pip
+pip install --upgrade pwntools
 
 # Pwndbg
 git clone https://github.com/pwndbg/pwndbg
 ./pwndbg/setup.sh
 
 # OneGadget
-sudo apt --assume-yes install ruby
+apt --assume-yes install ruby
 gem install one_gadget
 
 # GCC Multilib
-sudo apt --assume-yes install gcc-multilib
+apt --assume-yes install gcc-multilib
 
 # Z3
-pip3 install z3-solver
+pip install z3-solver
 
 # angr
-sudo apt --assume-yes install python3-dev libffi-dev build-essential virtualenvwrapper
-pip3 install angr
+apt --assume-yes install python3-dev libffi-dev build-essential virtualenvwrapper
+pip install angr
 
 # PyCryptodome
-pip3 install pycryptodome
+pip install pycryptodome
 
 # Go
 wget https://golang.org/dl/go1.15.7.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.15.7.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.15.7.linux-amd64.tar.gz
 rm go1.15.7.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
 source ~/.profile
@@ -102,10 +124,10 @@ go get github.com/tomnomnom/waybackurls
 go get -u github.com/sensepost/gowitness
 
 # ExifTool
-sudo apt --assume-yes install libimage-exiftool-perl
+apt --assume-yes install libimage-exiftool-perl
 
 # Binwalk
-sudo apt --assume-yes install binwalk
+apt --assume-yes install binwalk
 
 # jsteg
 go get lukechampine.com/jsteg
@@ -113,38 +135,47 @@ go get lukechampine.com/jsteg
 # zsteg
 gem install zsteg
 
-# Sherlock
-git clone https://github.com/sherlock-project/sherlock.git
-cd sherlock
-python3 -m pip install -r requirements.txt
-cd
-
 # Scapy
-pip3 install scapy
+pip install scapy
 
 # Nmap
-sudo apt --assume-yes install nmap
+apt --assume-yes install nmap
 
 # Nikto
-sudo apt --assume-yes install nikto
-
-# php-reverse-shell.php
-wget https://raw.githubusercontent.com/pentestmonkey/php-reverse-shell/master/php-reverse-shell.php 
-
-# rockyou.txt
-wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
-
-# SecLists
-git clone https://github.com/danielmiessler/SecLists.git
+apt --assume-yes install nikto
 
 # Updog
-pip3 install updog
+pip install updog
+
+#------------------#
+# Download as user |
+#------------------#
+
+# Switch to user
+exit
+cd
+mkdir Arsenal
+
+# Sherlock
+git clone https://github.com/sherlock-project/sherlock.git ~/Arsenal
+python -m pip install -r ~/Arsenal/sherlock/requirements.txt
+
+# php-reverse-shell.php
+mkdir ~/Arsenal/php-reverse-shell
+wget https://raw.githubusercontent.com/pentestmonkey/php-reverse-shell/master/php-reverse-shell.php -O ~/Arsenal/php-reverse-shell.php
+
+# rockyou.txt
+mkdir ~/Arsenal/rockyou
+wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt -O ~/Arsenal/rockyou/rockyou.txt
+
+# SecLists
+git clone https://github.com/danielmiessler/SecLists.git ~/Arsenal
 
 # WinPEAS and LinPEAS
-git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git
+git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git ~/Arsenal
 
 # pspy
-git clone https://github.com/DominicBreuker/pspy.git
+git clone https://github.com/DominicBreuker/pspy.git ~/Arsenal
 
 # ASCII art
 cat << "EOF"
