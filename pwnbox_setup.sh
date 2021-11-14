4a5740313f630279dd7d7fe04ab689160d6f931b
John Doe <ret2basic@gmail.com>
2021-11-13 23:01:46 -0500

    Update pwnbox_setup.sh

--- pwnbox_setup.sh
+++ pwnbox_setup.sh
@@ -62,8 +62,8 @@ apt --assume-yes install libc6-dbg gdb valgrind
 # Pwntools
 apt --assume-yes update
 apt --assume-yes install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential
-python3 -m pip install --upgrade pip
-python3 -m pip install --upgrade pwntools
+pip3 install --upgrade pip
+pip3 install 'pwntools>=4.7.0b0'
 
 # Pwndbg
 git clone https://github.com/pwndbg/pwndbg
